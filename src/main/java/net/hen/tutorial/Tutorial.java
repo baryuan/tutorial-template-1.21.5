907045e8d2dda0c6913750cef75dcd0cbaf9507d
Tie Hen <tiehen@Ties-MacBook-Air.local>
2025-05-05 11:39:45 -0400

    Initial commit

---
+++ src/main/java/net/hen/tutorial/Tutorial.java
@@ -0,0 +1,17 @@
+package net.hen.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorial implements ModInitializer {
+	public static final String MOD_ID = "tutorial";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/hen/tutorial/TutorialClient.java
@@ -0,0 +1,10 @@
+package net.hen.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
